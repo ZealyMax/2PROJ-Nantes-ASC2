0e47001fb0f7472f2e447c14ad4181e9d5d26edf
Arthur Ducept <48486020+arthurducept@users.noreply.github.com>
2020-05-04 01:52:49 +0200

    Design Login
    Début du design de la page Login.php :
    - 2 input et emplacement logo
    - background w/ blur

--- Développement/login.php
+++ Développement/login.php
@@ -7,30 +7,34 @@ include "connect_database.php";
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" type="text/css" href="login.css">
-        <title>Connexion</title>
+        <title>Connextion</title>
     </head>
     <body>
-        <div class="container">
-            <img id="logo" src="./Images/Logo/logoFull.png">
-            <div class="formPanel">
-                <form action="connect.php" method="post">
-                    <h1>Se connecter au créateur de formulaires</h1>
-                    <div class="formConnect">
-                        <input name="login" type="text" placeholder="Saisissez votre nom d'utilisateur">
-                        <input name="password" type="password" placeholder="Saisir le mot de passe">
-                        <input name="submit" type="submit" value="Se connecter">
+        <div name="Content">
+
+            <div class="bg-image"></div>
+
+            <div class="bg-login">
+
+                <div class="Logo">
+                    <div id="rectangle"></div>
+                    <p class="txt">Online Survey</p>
+                </div>
+
+                <form>
+
+                    <div class="Credentials">
+                        <input class="inputFields" name="login" type="text" placeholder="Username or email">
+                        <input class="inputFields" name="password" type="password" placeholder="Password">
                     </div>
-                    <hr>
-                    <div class="signin">
-                        <p>Vous n'avez pas encore de compte ?</p>
-                        <a href="signin.php">Créer un compte</a>
-                    </div>
+                    <div class="Other"></div>
                 </form>
+
             </div>
-            <div class="bgImg">
-                <img src="./Images/TDL.png">
-            </div>
+
         </div>
 
     </body>
 </html>
+
+<!---->
