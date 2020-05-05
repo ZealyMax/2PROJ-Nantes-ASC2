9706ef174f515f310df235a0bffcc80725f9677e
Arthur Ducept <48486020+arthurducept@users.noreply.github.com>
2020-05-05 13:33:16 +0200

    Home page
    Début de structure de la page

--- Développement/home.php
+++ Développement/home.php
@@ -2,17 +2,42 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=100%, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
         <title>Home</title>
     </head>
     <body>
-        <input type="button" onclick="location.href='survey_editor.php'" value="Créer un formulaire" />
-        <p>Liste des formulaires récents</p>
-        <p>Trier par:</p>
-        <nav>
-            <ul>
-
-            </ul>
-        </nav>
+        <div class=Header>
+            <div class=contentHead>
+                <div class=Logo>
+                    <div id=rectangle></div>
+                    <p class=txt>Online Survey</p>
+                </div>
+                
+                <div class=Recherche>
+                    <div class=rechBox>
+                        <input class=Search name="search" type="text" placeholder="Rechercher">
+                    </div>
+                </div>
+            
+                <div class=User>
+                    <div>
+                        Zeubi triste
+                    </div>
+                </div>
+            </div>
+            
+        </div>        
+        
+        <div class=Content>
+            <br>Test test plop <br>
+            
+        </div>        
+        
+        <div class=Footer>
+            Fils de pute
+        </div>        
     </body>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
 </html>
