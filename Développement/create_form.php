561abff4268db2385acf8ecaa958a4f05666b6d2
Arthur Ducept <48486020+arthurducept@users.noreply.github.com>
2020-05-10 19:23:40 +0200

    Modifs home page
    modifs.

--- Développement/create_form.php
+++ Développement/create_form.php
@@ -11,6 +11,9 @@
             $sql = "INSERT INTO surveys (title, id_users) VALUES ('$titre', $_SESSION[id_users])";  
 		}
         $res = mysqli_query($conn, $sql);
-        header("location:home.php")
-    }
+        #$sql = "SELECT id_surveys FROM surveys WHERE id_users = $_SESSION[id_users] ORDER BY DESC LIMIT 1";  
+        #$res = mysqli_query($conn, $sql);
+        #$result = $res->fetch_assoc();
+        header("location:home.php");
+        }
 ?>
--- Développement/survey_editor.js
+++ Développement/survey_editor.js
@@ -4,75 +4,75 @@ $(".btn-add").on('click', function(){
 });
 
 
-$("body").on('click', '.add-choice',function(){
-  var string = "<div><input type=radio><input placeholder=Option><button class=rm-div>X</button></div><button class=add-choice>Ajouter</button>"
+$(document).on('click', '.add-choice',function(){
+  var string = "<div><input type=radio><input name=multiple[] placeholder=Option><button class=rm-div>X</button></div><button class=add-choice>Ajouter</button>"
   $(this).parent().append(string);
   $(this).remove();
 } );
 
-$("body").on('click', '.add-check',function(){
-  var string = "<div><input type=checkbox><input placeholder=Option><button class=rm-div>X</button></div><button class=add-check>Ajouter</button>"
+$(document).on('click', '.add-check',function(){
+  var string = "<div><input type=checkbox><input name=checkbox[] placeholder=Option><button class=rm-div>X</button></div><button class=add-check>Ajouter</button>"
   $(this).parent().append(string);
   $(this).remove();
 } );
 
-$("body").on('click', '.add-list', function () {
-    var string = "<div><input placeholder=Option><button class=rm-div>X</button></div><button class=add-list>Ajouter</button>"
+$(document).on('click', '.add-list', function () {
+    var string = "<div><input name=list[] placeholder=Option><button class=rm-div>X</button></div><button class=add-list>Ajouter</button>"
     $(this).parent().append(string);
     $(this).remove();
 });
 
-$("body").on('click', '.add-line', function () {
+$(document).on('click', '.add-line', function () {
     var string = "<div><input placeholder=Ligne><button class=rm-div>X</button></div><button class=add-line-multiple>Ajouter</button>"
     $(this).parent().append(string);
     $(this).remove();
 });
 
-$("body").on('click', '.add-column-multiple', function () {
+$(document).on('click', '.add-column-multiple', function () {
     var string = "<div><input type=radio><input placeholder=Colonne><button class=rm-div>X</button></div><button class=add-column-multiple>Ajouter</button>"
     $(this).parent().append(string);
     $(this).remove();
 });
 
-$("body").on('click', '.add-column-checkbox', function () {
+$(document).on('click', '.add-column-checkbox', function () {
     var string = "<div><input type=checkbox><input placeholder=Colonne><button class=rm-div>X</button></div><button class=add-column-multiple>Ajouter</button>"
     $(this).parent().append(string);
     $(this).remove();
 });
 
-$("body").on('click', '.rm-div', function () {
+$(document).on('click', '.rm-div', function () {
     $(this).parent().remove();
 });
 
-$("body").on('click', 'option', function () {
-    if ($(this).val() == "short") {
-        $(this).parent().parent(".question").children(".question-content").html("<input placeholder=Réponse courte>");
+$(document).on('change', 'select', function () {
+    if ($(this).children('option:selected').val() == "short") {
+        $(this).parent(".question").children(".question-content").html("<input name=short[] placeholder='Réponse courte' READONLY>");
     }
-    if ($(this).val() == "long") {
-        $(this).parent().parent(".question").children(".question-content").html("<textarea placeholder=Réponse longue></textarea>");
+    if ($(this).children('option:selected').val() == "long") {
+        $(this).parent(".question").children(".question-content").html("<textarea name=long[] placeholder='Réponse longue' READONLY></textarea>");
     }
-    if ($(this).val() == "multiple") {
-        $(this).parent().parent(".question").children(".question-content").html("<input type=radio><input placeholder=Option><br><button class=add-choice>Ajouter</button></div>");
+    if ($(this).children('option:selected').val() == "multiple") {
+        $(this).parent(".question").children(".question-content").html("<input type=radio><input name=multiple[] placeholder=Option><br><button class=add-choice>Ajouter</button></div>");
     }
-    if ($(this).val() == "checkbox") {
-        $(this).parent().parent(".question").children(".question-content").html("<input type=checkbox><input placeholder=Option><br><button class=add-check>Ajouter</button></div>");
+    if ($(this).children('option:selected').val() == "checkbox") {
+        $(this).parent(".question").children(".question-content").html("<input name=checkbox[] type=checkbox><input placeholder=Option><br><button class=add-check>Ajouter</button></div>");
     }
-    if ($(this).val() == "list") {
-        $(this).parent().parent(".question").children(".question-content").html("<input placeholder=Option><br><button class=add-list>Ajouter</button></div>");
+    if ($(this).children('option:selected').val() == "list") {
+        $(this).parent(".question").children(".question-content").html("<input name=list[] placeholder=Option><br><button class=add-list>Ajouter</button></div>");
     }
-    if ($(this).val() == "linear-scale") {
-        $(this).parent().parent(".question").children(".question-content").html("<select class=min-scale><option>0</option><option>1</option></select><select class=max-scale><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option></select></div>");
+    if ($(this).children('option:selected').val() == "linear-scale") {
+        $(this).parent().parent(".question").children(".question-content").html("<select name=linear-scale[] class=min-scale><option>0</option><option>1</option></select><select class=max-scale><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option></select></div>");
     }
-    if ($(this).val() == "grid-multiple") {
+    if ($(this).children('option:selected').val() == "grid-multiple") {
         $(this).parent().parent(".question").children(".question-content").html("<div class=line><input placeholder=Ligne><button class=add-line>Ajouter</button></div><div class=column><input type=radio><input placeholder=Colonne><button class=add-column-multiple>Ajouter</button></div>");
     }
-    if ($(this).val() == "grid-checkbox") {
+    if ($(this).children('option:selected').val() == "grid-checkbox") {
         $(this).parent().parent(".question").children(".question-content").html("<div class=line><input placeholder=Ligne><button class=add-line>Ajouter</button></div><div class=column><input type=checkbox><input placeholder=Colonne><button class=add-column-checkbox>Ajouter</button></div>");
     }
-    if ($(this).val() == "date") {
-        $(this).parent().parent(".question").children(".question-content").html("<input placeholder=Date READONLY>");
+    if ($(this).children('option:selected').val() == "date") {
+        $(this).parent().parent(".question").children(".question-content").html("<input name=date[] placeholder=Date READONLY>");
     }
-    if ($(this).val() == "hour") {
-        $(this).parent().parent(".question").children(".question-content").html("<input placeholder=Heure READONLY>");
+    if ($(this).children('option:selected').val() == "hour") {
+        $(this).parent().parent(".question").children(".question-content").html("<input name=hour[] placeholder=Heure READONLY>");
     }
 });
