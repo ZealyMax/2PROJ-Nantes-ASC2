be504421d2fb4dd5aa4202881a8d103802a5b344
Zealy <maxime.germond@hotmail.com>
2020-06-10 01:41:21 +0200

    Update survey.sql

--- Developpement/fichierDB/survey.sql
+++ Developpement/fichierDB/survey.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 01 juin 2020 à 12:50
+-- Généré le :  mer. 10 juin 2020 à 01:40
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP :  7.4.1
 
@@ -25,6 +25,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `answers`
+--
+
+CREATE TABLE `answers` (
+  `id_answers` int(25) NOT NULL,
+  `id_surveys` int(25) NOT NULL,
+  `id_questions` int(25) NOT NULL,
+  `id_sub_questions` int(25) NOT NULL,
+  `answer` varchar(100) NOT NULL,
+  `numéro_feuille_réponse` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `questions`
 --
 
@@ -35,6 +50,40 @@ CREATE TABLE `questions` (
   `type` varchar(25) NOT NULL,
   `mustDo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `questions`
+--
+
+INSERT INTO `questions` (`id_questions`, `id_surveys`, `question`, `type`, `mustDo`) VALUES
+(237, 49, 'must', 'short', 0),
+(238, 49, 'pas must', 'short', 1),
+(239, 49, 'must', 'short', 0),
+(240, 50, 'RC (must)', 'short', 1),
+(241, 50, 'P (must)', 'long', 1),
+(242, 50, 'CM (must)', 'multiple', 1),
+(243, 50, 'CaC (must)', 'checkbox', 1),
+(244, 50, 'LD (must)', 'list', 1),
+(245, 50, 'EL 1-8 (must)', 'linear-scale', 0),
+(246, 50, 'GaCM (pas must)', 'grid-multiple', 0),
+(247, 50, 'GaCaC (pas must)', 'grid-checkbox', 0),
+(248, 50, 'D (must)', 'date', 0),
+(249, 50, 'H (must)', 'hour', 0),
+(274, 52, '', 'short', 1),
+(275, 52, '', 'short', 1),
+(276, 52, '', 'short', 1),
+(277, 52, '', 'short', 1),
+(278, 52, '', 'short', 1),
+(279, 52, '', 'short', 1),
+(280, 52, '', 'short', 1),
+(281, 52, '', 'short', 1),
+(282, 52, '', 'short', 1),
+(283, 52, '', 'short', 1),
+(284, 52, '', 'short', 1),
+(285, 52, '', 'short', 1),
+(286, 52, '', 'short', 1),
+(287, 52, '', 'short', 1),
+(288, 52, '', 'short', 1);
 
 -- --------------------------------------------------------
 
@@ -49,6 +98,26 @@ CREATE TABLE `sub_questions` (
   `value` varchar(25) CHARACTER SET latin7 COLLATE latin7_general_cs NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `sub_questions`
+--
+
+INSERT INTO `sub_questions` (`id_sub_questions`, `id_questions`, `type`, `value`) VALUES
+(298, 242, 'radio', 'CM1'),
+(299, 242, 'radio', 'CM2'),
+(300, 244, 'list', 'LD1'),
+(301, 244, 'list', 'LD2'),
+(302, 245, 'min-scale', '1'),
+(303, 245, 'max-scale', '8'),
+(304, 246, 'line', 'GaCM L1'),
+(305, 246, 'line', 'GaCM L2'),
+(306, 246, 'column-multiple', 'GaCM C1'),
+(307, 246, 'column-multiple', 'GaCM C2'),
+(308, 247, 'line', 'GaCaC L1'),
+(309, 247, 'line', 'GaCaC L2'),
+(310, 247, 'column-checkbox', 'GaCaC C1'),
+(311, 247, 'column-checkbox', 'GaCaC C2');
+
 -- --------------------------------------------------------
 
 --
@@ -59,8 +128,23 @@ CREATE TABLE `surveys` (
   `id_surveys` int(25) NOT NULL,
   `id_users` int(25) NOT NULL,
   `title` varchar(50) NOT NULL,
-  `description` varchar(100) NOT NULL
+  `description` varchar(100) NOT NULL,
+  `order_surveys` int(11) NOT NULL,
+  `date_ouverture` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `surveys`
+--
+
+INSERT INTO `surveys` (`id_surveys`, `id_users`, `title`, `description`, `order_surveys`, `date_ouverture`) VALUES
+(40, 1, 'plop', '', 0, '2020-06-06 00:00:00'),
+(41, 1, 'Test date', '', 0, '2020-06-01 02:07:05'),
+(43, 1, 'vide comme ta soeur', '', 0, '2020-06-02 20:30:08'),
+(44, 1, 'wallah ca se dit pas', 'ouais trop daccord', 0, '2020-06-09 20:34:02'),
+(49, 1, 'dla merde', '', 0, '2020-06-10 00:34:07'),
+(50, 1, 'test all questions (must / pas must) 2', 'this is a description', 0, '2020-06-10 00:34:41'),
+(52, 1, 'dla merde 2.0 (all must)', '', 0, '2020-06-10 00:36:45');
 
 -- --------------------------------------------------------
 
@@ -75,8 +159,22 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id_users`, `login`, `password`) VALUES
+(1, 'Zealy', 'Zealy');
+
+--
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`id_answers`),
+  ADD UNIQUE KEY `id_answers` (`id_answers`);
 
 --
 -- Index pour la table `questions`
@@ -114,28 +212,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pour la table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `id_answers` int(25) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id_questions` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_questions` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=289;
 
 --
 -- AUTO_INCREMENT pour la table `sub_questions`
 --
 ALTER TABLE `sub_questions`
-  MODIFY `id_sub_questions` int(25) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_sub_questions` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=312;
 
 --
 -- AUTO_INCREMENT pour la table `surveys`
 --
 ALTER TABLE `surveys`
-  MODIFY `id_surveys` int(25) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_surveys` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_users` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_users` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
